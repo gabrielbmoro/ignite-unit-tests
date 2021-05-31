d967eb7db00b6ea584d34b4e6afce337c65291b4
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2021-05-31 20:57:39 -0300

    Show user profile use case - tests

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+import { v4 as uuidV4 } from "uuid";
+import { AppError } from "../../../../shared/errors/AppError";
+
+let showUserProfileUseCase: ShowUserProfileUseCase;
+let userRepository: IUsersRepository;
+
+describe("ShowUserProfileUseCase", () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository();
+    showUserProfileUseCase = new ShowUserProfileUseCase(userRepository);
+  });
+
+  it("should be able to fetch the user profile information using valid id", async () => {
+    const user = await userRepository.create({
+      email: "test@test.com",
+      name: "test",
+      password: "test1234",
+    });
+
+    const userId = user.id ? user.id : "-1";
+
+    const profile = await showUserProfileUseCase.execute(userId);
+
+    expect(profile).toHaveProperty("email");
+    expect(profile).toHaveProperty("name");
+  });
+
+  it("should not be able to fetch the user profile information using invalid id", async () => {
+    expect(async () => {
+      await userRepository.create({
+        email: "test@test.com",
+        name: "test",
+        password: "test1234",
+      });
+
+      const userId = uuidV4();
+
+      await showUserProfileUseCase.execute(userId);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
